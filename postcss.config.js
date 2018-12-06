5680b1907497ad321a09137d6cccfccfcba1948b
Matt Billings <mbillings@gmail.com>
2018-12-06 09:05:09 -0800

    migrate to postcss preset env

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssUrl = require('postcss-url');
+const postcssImport = require('postcss-import');
+
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-url': {},
-    'postcss-cssnext': {
-      browsers: ['last 2 versions', '> 5%']
-    }
-  }
+  plugins: [
+    postcssImport(),
+    postcssUrl(),
+    postcssPresetEnv({
+      stage: 1,
+      features: {
+        'custom media queries': true,
+        'nesting-rules': true
+      }
+    })
+  ]
 };
